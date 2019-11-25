a52953d2f9510ce098aba674da7a0b65ba57d515
Calle Laakkonen <laakkonenc@gmail.com>
2019-11-25 22:44:29 +0200

    Remove unused PWA stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -13,7 +12,3 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
