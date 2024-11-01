ff1832fda682995ab56e666bd1e95012d2caf3f1
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-11-01 23:59:21 +0100

    Check for ext-auth setting definedness properly
    The closing bracket was in the wrong spot.

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -183,8 +183,8 @@ export default class extends React.Component {
 					<CheckboxInput label="Allow ext-auth ban exemptions" enabled={extAuthAvailable} {...vprops('extauthbanexempt')} />
 					<CheckboxInput label="Allow ext-auth ghosts" enabled={extAuthAvailable} {...vprops('extauthghosts')} />
 					<CheckboxInput label="Allow ext-auth web" enabled={extAuthAvailable && settings['extauthweb'] !== undefined} {...vprops('extauthweb')} />
-					<CheckboxInput label="Allow ext-auth web session" enabled={extAuthAvailable && settings['extauthwebsession' !== undefined]} {...vprops('extauthwebsession')} />
-					<CheckboxInput label="Allow ext-auth persist" enabled={extAuthAvailable && settings['extauthpersist' !== undefined]} {...vprops('extauthpersist')} />
+					<CheckboxInput label="Allow ext-auth web session" enabled={extAuthAvailable && settings['extauthwebsession'] !== undefined} {...vprops('extauthwebsession')} />
+					<CheckboxInput label="Allow ext-auth persist" enabled={extAuthAvailable && settings['extauthpersist'] !== undefined} {...vprops('extauthpersist')} />
 					<CheckboxInput label="Use ext-auth avatars" enabled={extAuthAvailable} {...vprops('extAuthAvatars')} />
 				</Field>
 
